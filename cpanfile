1efceb1dd2e193dc9cfb1805cbc27e6207cadfdb
Sapphire Paw <devel@sapphirepaw.org>
2013-09-19 11:09:11 -0400

    Remove red herrings.

--- cpanfile
+++ cpanfile
@@ -5,5 +5,4 @@ requires 'MIME::Types';
 requires 'Test::More';
 requires 'YAML';
 requires 'Dancer', '>= 1.3118, < 2.0000';
-requires 'Starman', '>= 0.4000, < 1.0';
 requires 'Plack', '>= 1.0028';
--- testapp/bin/start.pl
+++ testapp/bin/start.pl
@@ -8,9 +8,8 @@ use FindBin qw($Bin);
 use File::Basename qw(dirname);
 
 my $APPDIR = dirname($Bin);
-my $LISTEN = '127.0.0.1:5001';
 
-my @cmd = (qw(plackup -s Starman --listen), $LISTEN, "$APPDIR/bin/app.pl");
+my @cmd = ('plackup', "$APPDIR/bin/app.pl");
 say STDERR "start.pl using Perl $]";
 say STDERR "@cmd";
 
